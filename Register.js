f242817906477c7da383defadc02764b83b06a5f
Akib Ali <akib7599@gmail.com>
2022-08-16 18:25:13 +0530

    ok

---
+++ Register.js
@@ -0,0 +1,111 @@
+var userdata = JSON.parse(localStorage.getItem("userdatabase")) || [];
+
+document.querySelector('.addressform').addEventListener('submit', adddata)
+
+//document.querySelector(".myform").addEventListener("submit", adddata);
+
+
+
+function adddata(event){
+
+    event.preventDefault()
+
+    var email = document.getElementById("email").value;
+    var firstname = document.getElementById("firstname").value;
+    var address = document.getElementById("address").value;
+    // var city = document.getElementById("city").value;
+  //  var state = document.getElementById("select_state").value;
+    // var pincode = document.getElementById("pincode").value;
+    // var phone = document.getElementById("phone").value;
+
+   if(email == ""){
+
+    document.getElementById("emailid").innerHTML="Enter a valid email"
+    return false;
+ }
+
+
+
+ else if(firstname == ""){
+    document.getElementById("firstnamediv").innerHTML = "Enter an First name"
+    return false;
+   }
+  
+  
+
+ else if(address == ""){
+  document.getElementById("youraddress").innerHTML = "**Enter an address "
+ // return false;
+ }
+
+
+//  else if(city == ""){
+//  document.getElementById("citydiv").innerHTML = " Enter a city"
+//  return false;
+//  }
+
+
+//  else if(pincode == ""){
+//     document.getElementById("pincodediv").innerHTML = "Enter a PIN code"
+//     return false;
+//     }
+   
+
+//     else if(phone == ""){
+//         document.getElementById("phonediv").innerHTML = "Enter a phone number to use this delivery method"
+//         return false;
+//         }
+    
+
+ 
+
+    console.log("success")
+
+
+
+
+    var email = document.querySelector("#email").value;
+    var address= document.querySelector("#address").value;
+
+
+    var userinformation={
+
+        email:email,
+        address:address,
+    };
+
+
+    userdata.push(userinformation);
+
+    localStorage.setItem("userdatabase",JSON.stringify(userdata))
+
+
+
+
+    window.location.href="Login.html"
+}
+
+
+
+
+
+
+
+//checking
+
+
+ document.querySelector("#checkbtn").addEventListener("click", crosscheck);
+
+
+
+// var registeruser = JSON.parse(localStorage.getItem("userdatabase"))
+
+
+ function crosscheck(){
+
+    console.log("successbtn")
+
+ }
+//     var checkemail = document.querySelector("#emailcheck").value;
+
+//     var checkaddress= document.querySelector("#addresscheck").value;
